2fd4057c0ada4de06cce347a7bc5fa1d0dae53cd
Dima Kuzko <bigmandarine1991@gmail.com>
2022-10-24 23:45:17 +0300

    Phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    name: '',
+  };
+  handleChange = evt => {
+    this.setState({ [evt.currentTarget.name]: evt.currentTarget.value });
+  };
+  addContact = data => {
+    data.id = nanoid();
+    const { contacts } = this.state;
+    const checkContact = contacts.find(contact => contact.name === data.name);
+    checkContact
+      ? alert(`${data.name} is already in the contacts`)
+      : this.setState(prevState => ({
+          contacts: [...prevState.contacts, data],
+        }));
+  };
+  onDeleteContact = idContact => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => idContact !== contact.id),
+    }));
+  };
+
+  render() {
+    const { contacts } = this.state;
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <div>
+          <h2>Contacts</h2>
+          <ContactList
+            contacts={contacts}
+            onDeleteContact={this.onDeleteContact}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+export const ContactList = ({ contacts, onDeleteContact }) => {
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => (
+        <li key={id}>
+          {name}: {number}
+          <button onClick={() => onDeleteContact(id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
